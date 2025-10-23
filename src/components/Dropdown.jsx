a4c9e17bcb53fb040d6428ddd2dbe3e79c8a3d71
Caroline Reiss <38852967+CoraNiall@users.noreply.github.com>
2025-10-23 21:44:10 +0100

    update dropdown text

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -8,7 +8,7 @@ export default function Dropdown({ children, onAddTip }) {
       <select id="question1" onChange={e => onAddTip(e.target.value)}>
         <option value="0">Select an option...</option>
         <option value="5">Not at all</option>
-        <option value="10">It was good. I liked it</option>
+        <option value="10">It was all right. I liked it</option>
         <option value="20">It was fantastic. Best ever</option>
       </select>
     </li>
